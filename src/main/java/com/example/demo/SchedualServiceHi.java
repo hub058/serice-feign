15dd348ac2b6fdd16ae2e91719ca0eb5b45b61b8
endon058 <877058128@qq.com>
2018-06-23 10:47:00 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/SchedualServiceHi.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Administrator on 2018/6/23.
+ * 定义一个feign接口，通过@ FeignClient（“服务名”），
+ * 来指定调用哪个服务。比如在代码中调用了service-hi服务的“/hi”接口
+ */
+
+@FeignClient("service-hi")
+@Service
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam("name") String name);
+
+}
